6cd27abb64cdbeb9094cdec2fc57140d083d9657
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2021-09-07 12:06:23 +0500

    Added Screens Folder

--- App.js
+++ App.js
@@ -1,37 +1,9 @@
 import React from 'react';
-import {Text, View, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-const HomeScreen = ({navigation}) => {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to details screen"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
-  );
-};
-
-const DetailsScreen = ({navigation}) => {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to details screen...again"
-        onPress={() => navigation.push('Details')}
-      />
-      <Button title="Go to home" onPress={() => navigation.navigate('Home')} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-      <Button
-        title="Go to the first screen"
-        onPress={() => navigation.popToTop()}
-      />
-    </View>
-  );
-};
+import HomeScreen from './Screens/HomeScreen';
+import DetailsScreen from './Screens/DetailsScreen';
 
 const Stack = createNativeStackNavigator();
 
